9a8848af28dea7ea8cd0719eb1ef04f20dda95fc
Arik Alexander <arik@mail.com>
2021-03-29 07:54:31 -0700

    list

---
+++ include/list.h
@@ -0,0 +1,24 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+typedef struct list_node list_node_t;
+
+typedef struct {
+	int (*cmp)(void *d1, void * d2);
+	void (*print)(void *d);
+} list_ops_t;
+
+typedef struct {
+	list_node_t *head;
+	list_ops_t ops;
+} list_t;
+
+list_t *list_new(list_ops_t *ops);
+int list_add_head(list_t *l, void *data);
+int list_add_tail(list_t *l, void *data);
+void list_print(list_t *l);
+void list_exec(list_t *l, void (*cb)(void *d));
+void *list_pop(list_t *l, int id);
+void *list_get(list_t *l, void *d);
+
+#endif
---
+++ src/list.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+struct list_node {
+        void *data;
+        struct list_node *next;
+};
+
+list_t *list_new(list_ops_t *ops)
+{
+	list_t *new_list;
+
+	if (!(new_list = malloc(sizeof(list_t))))
+		return NULL;
+	memset(new_list, 0, sizeof(list_t));
+	new_list->ops = *ops;
+
+	return new_list;
+}
+
+int list_add_head(list_t *l, void *data)
+{
+	list_node_t *new_node;
+
+	if (!(new_node = malloc(sizeof(list_node_t))))
+		return -1;
+	new_node->data = data;
+	new_node->next = l->head;
+	l->head = new_node;
+
+	return 0;
+}
+
+int list_add_tail(list_t *l, void *data)
+{
+	list_node_t *new_node, **p;
+
+	if (!(new_node = malloc(sizeof(list_node_t))))
+		return -1;
+	new_node->data = data;
+	new_node->next = NULL;
+	for (p = &(l->head); *p; p = &((*p)->next));
+	*p = new_node;
+
+	return 0;
+}
+
+void list_print(list_t *l)
+{
+	list_node_t *p;
+
+	for (p = l->head; p; p = p->next) { 
+		if (l->ops.print)
+			l->ops.print(p->data);
+		else
+			printf("%d \n", (int)(long)(p->data));
+	}
+}
+
+void list_exec(list_t *l, void (*cb)(void *d))
+{
+	list_node_t *p;
+
+	for (p = l->head; p; p = p->next) { 
+		if (cb)
+			cb(p->data);
+	}
+}
+
+void *list_pop(list_t *l, int id)
+{
+	list_node_t **p;
+	void *retval, *del;
+	int i;
+
+	if (id == -1) {
+		id = 0;
+		for (p = &(l->head); *p; p = &((*p)->next))
+			id++;
+		id--;
+	}
+
+	for (p = &(l->head), i = 0; *p && i < id; p = &((*p)->next), i++);
+	if (i < id)
+		return NULL;
+
+	if (!*p)
+		return NULL;
+	retval = (*p)->data;
+	del = *p;
+	*p = (*p)->next;
+	free(del);
+
+	return retval;
+}
+
+void *list_get(list_t *l, void *d)
+{
+	list_node_t *p;
+
+	if (!(l->ops.cmp))
+		return NULL;
+
+	for (p = l->head; p; p = p->next) {
+		if (l->ops.cmp(d, p->data) == 0)
+			return p->data;
+	}
+
+	return NULL;
+}
+
